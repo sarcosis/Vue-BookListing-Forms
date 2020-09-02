5759beb1cf0f41512d800f6145de57483d0cdf85
Victor Lam <victorlam03@gmail.com>
2020-09-01 21:14:51 -0400

    End of Module 2 (task 3)

--- src/components/BookForm.vue
+++ src/components/BookForm.vue
@@ -4,9 +4,11 @@
     <input v-model="bookData.bookAuthor" type="text" name="author" value="" placeholder="Book Author">
     <button type="submit" name="button">Add Book</button>
     <div>
-      <label for="finishedReading">Finished Reading</label><input v-model="bookData.finishedReading" type="checkbox">
-      <label for="borrowed">Borrowed</label><input v-model="bookData.ownership" type="radio" value="borrowed">
-      <label for="bought">Bought</label><input v-model="bookData.ownership" type="radio" value="bought">
+      <ul>
+        <li><label for="finishedReading">Finished Reading </label><input v-model="bookData.finishedReading" type="checkbox"></li>
+      <li><label for="borrowed">Borrowed</label><input v-model="bookData.ownership" type="radio" value="borrowed">
+      <label for="bought">Bought</label><input v-model="bookData.ownership" type="radio" value="bought"></li>
+      </ul>
   </div>
   </form>
 
